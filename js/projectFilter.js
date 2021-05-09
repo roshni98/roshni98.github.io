ab615c0abce0080c2d3070067a779b98381b530b
Roshni <roshnishah318@gmail.com>
2021-05-09 16:21:55 -0400

    Initial commit

---
+++ js/projectFilter.js
@@ -0,0 +1,19 @@
+$(function () {
+	var filterList = {
+		init: function () {
+			// MixItUp plugin
+			// http://mixitup.io
+			$('.portfolio-grid').mixItUp({
+				selectors: {
+  			  target: '.portfolio',
+  			  filter: '.filter'
+  		  },
+  		  load: {
+    		  filter: 'all' // show app tab on first load
+    		}
+			});
+		}
+	};
+	// Run the show!
+	filterList.init();
+});
